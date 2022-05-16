52fdf52e39ecf09be9ea4a252630ac7a2a70f6de
tepolucas <shaunters545@gmail.com>
2022-05-15 21:46:20 -0500

    modification of the numeric variable

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = 'hola';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = '5';
+const nuevoNum = 'edad 21';
 
 // Crea una variable booleana:
 const nuevoBool = null;
